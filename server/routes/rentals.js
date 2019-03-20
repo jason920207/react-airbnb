d164859744640975bf0e056caeb4823cedf3ed32
xiaojie zhang <xiaojiezhang@xiaojiedeMacBook-Air.local>
2019-03-20 08:14:12 -0400

    add basic server setup, add rental routes, add seeddb

---
+++ server/routes/rentals.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router()
+const Rental = require('../models/rental')
+
+router.get('',function(req,res) {
+  Rental.find({}, function(err, rentals){
+    res.json(rentals)
+  })
+})
+
+router.get('/:id',function(req,res) {
+  const rentalId = req.params.id
+  Rental.findById(rentalId, function(err, rental){
+    if(err){
+      res.status(422).send({errors: [{title:'Rental Error',detail:'Could not find Rental'}]})
+    }
+    res.json(rental)
+  })
+})
+
+module.exports = router
