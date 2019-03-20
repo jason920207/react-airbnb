78e80ba2ad7838b5802891dc1fb47a7d32a5d195
xiaojie zhang <xiaojiezhang@xiaojiedeMacBook-Air.local>
2019-03-20 18:06:50 -0400

    add cache

--- src/components/map/GoogleMap.js
+++ src/components/map/GoogleMap.js
@@ -5,6 +5,7 @@ import {
   GoogleMap,
   Marker,
 } from "react-google-maps";
+import { Cacher } from '../../services/cacher'
 
 function MapComponent(props) {
 
@@ -26,7 +27,7 @@ function withGeocode(WrappedComponent) {
     return class extends React.Component {
       constructor() {
         super()
-
+        this.cacher = new Cacher()
         this.state={
           coordinates:{
             lat:0,
@@ -42,13 +43,19 @@ function withGeocode(WrappedComponent) {
       geocodeLocation() {
         const location = this.props.location
         const geocoder = new window.google.maps.Geocoder()
-        geocoder.geocode({address: location},(result, status)=>{
-          if(status==='OK') {
-            const geometry= result[0].geometry.location
-            const coordinates = { lat:geometry.lat(),lng: geometry.lng()}
-            this.setState({coordinates})
-          }
-        })
+
+        if (this.cacher.isValueCached(location)) {
+          this.setState({coordinates: this.cacher.getCachedValue(location)})
+        } else {
+          geocoder.geocode({address: location},(result, status)=>{
+            if(status==='OK') {
+              const geometry= result[0].geometry.location
+              const coordinates = { lat:geometry.lat(),lng: geometry.lng()}
+              this.cacher.cacheValue(location,coordinates)
+              this.setState({coordinates})
+            }
+          })
+        }
       }
       render() {
         return(
